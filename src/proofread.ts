7d5e40e9d75d58d5216322836871cacd2365ac64
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-03-25 21:04:34 +0100

    feat: create diff from openai response & turn into markdown

--- src/proofread.ts
+++ src/proofread.ts
@@ -1,12 +1,22 @@
+import { diffWords } from "diff";
 import { Editor, Notice, requestUrl } from "obsidian";
 import { OPENAI_API_KEY, OPENAI_MODEL, STATIC_PROMPT } from "./settings";
 
-export async function proofreadParagraph(editor: Editor): Promise<void> {
-	new Notice("Sending proofread request…");
-	const cursor = editor.getCursor();
-	const currentParagraph = editor.getLine(cursor.line);
-	const text = currentParagraph;
+// DOCS https://github.com/kpdecker/jsdiff#readme
+/* -> additions as ==highlights== & removals as ~~strikethroughs~~ */
+function getDiffMarkdown(oldText: string, newText: string): string {
+	const changes = diffWords(oldText, newText)
+		.map((part) => {
+			if (part.added) return `==${part.value}==`;
+			if (part.removed) return `~~${part.value}~~`;
+			return part.value;
+		})
+		.join("");
 
+	return changes;
+}
+
+async function openAiRequest(oldText: string): Promise<string | undefined> {
 	// DOCS https://platform.openai.com/docs/api-reference/chat
 	const response = await requestUrl({
 		url: "https://api.openai.com/v1/chat/completions",
@@ -16,14 +26,29 @@ export async function proofreadParagraph(editor: Editor): Promise<void> {
 		headers: { Authorization: "Bearer " + OPENAI_API_KEY },
 		body: JSON.stringify({
 			model: OPENAI_MODEL,
-			messages: [{ role: "user", content: STATIC_PROMPT + text }],
+			messages: [{ role: "user", content: STATIC_PROMPT + oldText }],
 		}),
 	});
+
 	const error = response.json.error;
 	if (error) {
-		new Notice(`ERROR ${error.code}: ${error.message}`, 0);
+		const msg = `ERROR ${error.code}: ${error.message}`;
+		console.error(msg);
+		new Notice(msg, 4000);
 		return;
 	}
-	const answer = response.json.choices[0].message.content;
-	new Notice(`answer: ${answer}`, 0);
+	const newText = response.json.choices[0].message.content;
+	return newText;
+}
+
+export async function proofreadParagraph(editor: Editor): Promise<void> {
+	new Notice("Sending proofread request…");
+	const cursor = editor.getCursor();
+	const oldText = editor.getLine(cursor.line);
+	const newText = await openAiRequest(oldText);
+	if (!newText) return;
+
+	const changes = getDiffMarkdown(oldText, newText);
+	editor.setLine(cursor.line, changes);
+	editor.setCursor(cursor);
 }
