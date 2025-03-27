96122ff0980d84af20b0357feeb3eb6cad196953
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-03-27 16:28:35 +0100

    improv: still save response exceeding max output tokens, in the console

--- src/proofread.ts
+++ src/proofread.ts
@@ -80,20 +80,6 @@ async function openAiRequest(
 	}
 	notice.hide();
 
-	// GUARD text was too long, so we abort to prevent a cut-off text to be used
-	// for diff creation. (There is also a length check before the request is
-	// send, but it is based on estimates, and there is thus a small chance that
-	// the actual text is too long.)
-	const outputTokensUsed = response.json?.usage?.completion_tokens || 0;
-	if (outputTokensUsed > OPENAI_MODEL.maxOutputTokens) {
-		const tokensCutOff = outputTokensUsed - OPENAI_MODEL.maxOutputTokens;
-		const msg =
-			`The ${scope} is ~${tokensCutOff} tokens too long.\n\n` +
-			`The maximum length is ~${OPENAI_MODEL.maxOutputTokens} tokens.`;
-		new Notice(msg, 6000);
-		return;
-	}
-
 	// HANDLE RESPONSE
 	let newText = response.json?.choices?.[0].message.content;
 	if (!newText) {
@@ -102,7 +88,25 @@ async function openAiRequest(
 		return;
 	}
 
-	// Ensure same amount of surrounding whitespace.
+	// GUARD text too long
+	// abort to prevent a cut-off text to be used for diff creation. (There is
+	// also a length check before the request is send, but it is based on
+	// estimates, and thus a small chance that the actual text is too long.)
+	const outputTokensUsed = response.json?.usage?.completion_tokens || 0;
+	if (outputTokensUsed > OPENAI_MODEL.maxOutputTokens) {
+		const tokensCutOff = outputTokensUsed - OPENAI_MODEL.maxOutputTokens;
+		const msg =
+			`The ${scope} is ~${tokensCutOff} tokens too long to create suggestions. ` +
+			`The maximum length is ~${OPENAI_MODEL.maxOutputTokens} tokens.` +
+			"\n\n" +
+			"The cut-off, raw response-text can be retrieved from in the console (ctrl+shift+i / cmd+opt+i).";
+		new Notice(msg, 10000);
+
+		// save text in console, since the request still incurred a certain cost
+		navigator.clipboard.writeText(newText);
+	}
+
+	// Ensure same amount of surrounding whitespace
 	// (A selection can have surrounding whitespace, but the AI response usually
 	// removes those. This results the text effectively being trimmed.)
 	const leadingWhitespace = oldText.match(/^(\s*)/)?.[0] || "";
