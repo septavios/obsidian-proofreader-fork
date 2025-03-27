8be1cfeb0de435f99b14abab63ab036c748a9bd5
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-03-27 12:47:15 +0100

    refactor: simplify accept-reject main function

--- src/accept-reject-suggestions.ts
+++ src/accept-reject-suggestions.ts
@@ -1,18 +1,20 @@
 import { Editor } from "obsidian";
 
-export function acceptOrReject(editor: Editor, mode: "accept" | "reject"): void {
+export function acceptOrRejectInText(editor: Editor, mode: "accept" | "reject"): void {
 	const selection = editor.getSelection();
-	const { line, ch } = editor.getCursor();
+	const cursor = editor.getCursor();
 
-	const text = selection || editor.getLine(line);
+	const text = selection || editor.getLine(cursor.line);
 	const updatedText =
 		mode === "accept"
 			? text.replace(/==/g, "").replace(/~~.*?~~/g, "")
 			: text.replace(/~~/g, "").replace(/==.*?==/g, "");
 	if (selection) editor.replaceSelection(updatedText);
-	else editor.setLine(line, updatedText);
+	else editor.setLine(cursor.line, updatedText);
 
 	// keep cursor location
 	const charsLess = text.length - updatedText.length;
-	editor.setCursor({ line: line, ch: Math.max(ch - charsLess, 0) });
+	cursor.ch = Math.max(cursor.ch - charsLess, 0);
+	editor.setCursor(cursor);
+}
 }
