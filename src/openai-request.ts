235733b960ad182fad1521658d4e7dee96ef037c
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-17 13:21:27 +0200

    fix: error handling on mobile
    https://github.com/obsidianmd/obsidian-releases/pull/5832#issuecomment-2812523089

--- src/openai-request.ts
+++ src/openai-request.ts
@@ -1,10 +1,16 @@
-import { Notice, RequestUrlResponse, requestUrl } from "obsidian";
+import { Notice, Platform, RequestUrlResponse, requestUrl } from "obsidian";
 import { MODEL_SPECS, ProofreaderSettings } from "./settings";
 
 function logError(obj: unknown): void {
-	const hotkey = process.platform === "darwin" ? "cmd+opt+i" : "ctrl+shift+i";
-	new Notice(`Error. Check the console for more details (${hotkey}).`);
-	console.error("[Proofreader plugin] error", obj);
+	if (Platform.isMobileApp) {
+		// No issue way of checking the logs on mobile, thus recommending to
+		// retrieve error via running on desktop instead.
+		new Notice("Error. For details, run the respective function on the desktop.");
+	} else {
+		const hotkey = Platform.isMacOS ? "cmd+opt+i" : "ctrl+shift+i";
+		new Notice(`Error. Check the console for more details (${hotkey}).`);
+		console.error("[Proofreader plugin] error", obj);
+	}
 }
 
 export async function openAiRequest(
